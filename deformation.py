548c1f60e842dea7743394feb693a95fb9f3d881
xinyi zhang <xinyi.zhang@hotmail.it>
2020-12-17 20:36:26 +0100

    changed Q matrix calculation adds M matrix calculation

--- deformation.py
+++ deformation.py
@@ -13,7 +13,8 @@ def find_triangle_fan(vi, mesh):
 def load_deformation(n, name):
     result = []
     for i in range(1, n + 1):
-        result.append(pymesh.load_mesh("{}-{:02d}.obj".format(name, i)))
+        # result.append(pymesh.load_mesh("{}-{:02d}.obj".format(name, i)))
+        result.append(pymesh.load_mesh(name))
     return result
 
 
@@ -30,27 +31,39 @@ def find_source_trans_matrix(mesh, deform):
 
 
 def find_triangle_matrix(mesh):
-    #B = V^(-t)
+    # B = V^(-t)
     V = [(triangle3_to_matrix(mesh.vertices[triangle])) for triangle in mesh.faces]
     V_t = [(v.transpose()) for v in V]
     V_t_inv = [(np.linalg.inv(v_t)) for v_t in V_t]
     V_t_inv_matrix = scipy.linalg.block_diag(*V_t_inv)
-    Q_matrix = find_realignment_matrix(len(V) // 4)
-    # result = BQMQ^(-1)DP
-    result = V_t_inv_matrix @ Q_matrix
+    Q_matrix = find_realignment_Q_matrix(len(V) // 3)
+    M_matrix = find_vector_to_point_matrix(len(V))
+    # result = BQM(Q^)DP
+    result = V_t_inv_matrix @ Q_matrix @ M_matrix
     return result
 
-#the vertices are given in the form of (v1x, v1y, v1z, v2x......v4z) for each triangle change it to (v1x, v2x, v3x, v4x...)
-def find_realignment_matrix(n):
+# the original matrix uses v1 v2 v3 which are differences of points, now function inverses the process
+def find_vector_to_point_matrix(n):
+    m = np.array([[-1, 1, 0, 0], [-1, 0, 1, 0], [-1, 0, 0, 1]])
+    ms = []
+    for i in range(0, n):
+        ms.append(m)
+    M = scipy.linalg.block_diag(*ms)
+    return M
+
+
+# the vertices are given in the form of (v1x, v1y, v1z, v2x......v4z) for each triangle change it to (v1x, v2x, v3x,
+# v4x...)
+def find_realignment_Q_matrix(n):
     lines = []
-    for i in range(0, 12):
-        line = np.zeros(12)
-        if i < 4:
+    for i in range(0, 9):
+        line = np.zeros(9)
+        if i < 3:
             line[i * 3] = 1
-        if 4 <= i < 8:
-            line[1 + ((i * 3) % 12)] = 1
-        if 8 <= i < 12:
-            line[2 + ((i * 3) % 12)] = 1
+        if 3 <= i < 6:
+            line[1 + ((i * 3) % 9)] = 1
+        if 6 <= i < 9:
+            line[2 + ((i * 3) % 9)] = 1
         lines.append(line)
     q = np.array(lines)
     qs = []
@@ -70,6 +83,8 @@ def find_target_trans_matrix(target_mesh):
     # D is the matrix of vertices correlation
     # B is the matrix of the target mesh vectors
     # M is the transformation of the vector coordinate in the original point coordinate
+
+
 def main(source, deform_s, target):
     n, deform_name = deform_s
     source_deforms = load_deformation(n, deform_name)
@@ -81,4 +96,4 @@ def main(source, deform_s, target):
 
 
 if __name__ == '__main__':
-    main("horse_ref_decimate.obj", (2, "res/horse-gallop/horse-gallop"), "camel_ref_decimate.obj")
+    main("horse_ref_decimate.obj", (2, "res/horse-gallop/horse-gallop-01_decimated.obj"), "camel_ref_decimate.obj")
